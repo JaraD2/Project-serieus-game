693926ff10ec0139ab012af796083f0475b4487a
Jara Doornbos <81758642+JaraD2@users.noreply.github.com>
2023-12-11 20:17:55 +0100

    Addad text animation script

--- script.js
+++ script.js
@@ -3,95 +3,78 @@ document.addEventListener('DOMContentLoaded', function () {
   const house1 = document.getElementById('house1');
   const house2 = document.getElementById('house2');
   const house3 = document.getElementById('house3');
-  const houses = [house1, house2, house3];
   const back = document.getElementById('back');
-  var previous = background.getAttribute('src');
+  let previous = background.getAttribute('src');
+  let location = 0;
+  const nextText = document.getElementById('nextText');
 
   house1.addEventListener('click', function() {
-      console.log('house1');
-      previous = background.getAttribute('src');
-      console.log(previous);
-      background.setAttribute('src', './img/er.png');
-      hideOrShowHouses('hide');
+    previous = background.getAttribute('src');
+    background.setAttribute('src', './img/er.png');
+    location = 1;
+    hideOrShowHouses('hide');
+    textAnimation(location);
   });
+
   house2.addEventListener('click', function() {
-      console.log('house2');
+    location = 2;
+    textAnimation(location);
   });
+
   house3.addEventListener('click', function() {
-      console.log('house3');
+    location = 3;
+    textAnimation(location);
   });
 
   back.addEventListener('click', function() {
-      console.log('back');
-      background.setAttribute('src', previous);
-      hideOrShowHouses('show');
+    background.setAttribute('src', previous);
+    hideOrShowHouses('show');
   });
 
-
-
   function hideOrShowHouses(option) {
-    if (option === 'hide') {
-      for (let i = 0; i < houses.length; i++) {
-        houses[i].classList.add('hidden');
-        console.log('hidden');
-        console.log(houses[i]);
+    const houses = [house1, house2, house3];
+    houses.forEach(house => {
+      if (option === 'hide') {
+        house.classList.add('hidden');
+      } else if (option === 'show') {
+        house.classList.remove('hidden');
       }
-    } else if (option === 'show') {
-      for (let i = 0; i < houses.length; i++) {
-        houses[i].classList.remove('hidden');
-        console.log('show');
-      }
-    }}
-});
+    });
+  }
 
+  let jsonData;
+  fetch('./text.json')
+    .then(response => response.json())
+    .then(data => {
+      jsonData = data;
+      console.log(jsonData);
+    })
+    .catch(error => {
+      console.log('Error fetching JSON:', error);
+    });
 
-
-
-
-
-
-  
-    // function specialEffectsSwitch(i) {
-    //   switch (i) {
-    //     case 1:
-    //       console.log('case 1');
-    //       break;
-    //     case 2:
-    //       console.log('case 2');
-    //       specialEffects(body, 'shake');
-    //       break;
-    //     case 3:
-    //       console.log('case 3');
-    //       break;
-    //     case 4:
-    //       console.log('case 4');
-    //       break;
-    //     default:
-    //       console.log('default');
-    //   }
-    // }
-  
-  
-    function textAnimation(currentImage) {
-      const text = jsonData[currentImage].text.split('');
-      console.log(text);
-      console.log(text.length);
-      var running = false;
+  function textAnimation(currentLocation) {
+    if (jsonData) {
+      const text = jsonData[`char${currentLocation}`][`${location}`].split('');
+      const displayedText = document.getElementById('displayedText');
+      displayedText.textContent = ''; // Clear previous text
       let i = 0;
       const timer = setInterval(function () {
         if (i < text.length) {
           displayedText.textContent += text[i];
+          console.log(text[i]);
           i++;
+        } else {
+          clearInterval(timer);
         }
-        
       }, 75);
     }
-  
-    
-    function specialEffects(element, effect) {
+  }
+
+  function specialEffects(element, effect) {
+    element.classList.toggle(effect);
+    setTimeout(function () {
       element.classList.toggle(effect);
-      setTimeout(function () {
-        element.classList.toggle(effect);
-      }, 1000);
-    }
-  
+    }, 1000);
+  }
+});
